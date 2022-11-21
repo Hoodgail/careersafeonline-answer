2baf3fac3915a5a0ed417ed0ecc1e75d4126e3ec
Benjamin <45743294+Hoodgail@users.noreply.github.com>
2022-11-21 16:16:33 +0000

    p

--- ma.6.js
+++ ma.6.js
@@ -75,11 +75,11 @@ function multipleAnswers() {
 
             console.log(KCObject._ansKey, KCObject._ansKey[currentQuestion], i, correct)
 
-            // if (correct) {
-            //     setTimeout(() => {
-            //         toggleSelect(i)
-            //     }, 1000)
-            // }
+            if (correct) {
+                setTimeout(() => {
+                    toggleSelect(i)
+                }, 1000)
+            }
 
             //write out only the amount of options available
             $('.question-options').append(`<div style="${correct ? correctStyle : incorrectStyle}" class=\"ma-opt-container\"><div class=\"ma-cont\"><div class=\"ma-selector\"></div><div class=\"ma-btn-container\"><button type=\"button\" class=\"btn btn-lg activityBtns ma-btn-options option` + i + "\" aria-pressed=\"false\" onclick=\"toggleSelect(" + i + "); return false;\">" + optionsArray[currentQuestion][i] + "</button></div></div></div>");
